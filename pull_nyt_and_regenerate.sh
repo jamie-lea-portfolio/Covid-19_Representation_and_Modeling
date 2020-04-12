aace2da6e1d287975fa8a4c9169c00df1d1e96ec
jml-happy <17792773+jml-happy@users.noreply.github.com>
2020-04-12 08:31:30 -0500

    auto update animation

--- pull_nyt_and_regenerate.sh
+++ pull_nyt_and_regenerate.sh
@@ -5,11 +5,11 @@ echo "Script started" >> /tmp/debug_cron
 export DISPLAY=:0
 # use keychain so the ssh key doesn't have to be unlocked
 . ~/.keychain/`/bin/hostname`-sh
-cd /home/happy/deep_learning/covid_graph_model/nyt_repo && git pull
-cd /home/happy/deep_learning/covid_graph_model/python_scripts && python3 clean_nyt_county_data.py && python3 nytimes_render_spread-optimize.py
-cd /home/happy/deep_learning/covid_graph_model-resume/images
-rm current_covid_spread-animated.gif
-ln /home/happy/deep_learning/covid_graph_model/images/current_covid_spread-animated.gif
+# cd /home/happy/deep_learning/covid_graph_model/nyt_repo && git pull
+# cd /home/happy/deep_learning/covid_graph_model/python_scripts && python3 clean_nyt_county_data.py && python3 nytimes_render_spread-optimize.py
+# cd /home/happy/deep_learning/covid_graph_model-resume/images
+# rm current_covid_spread-animated.gif
+# ln /home/happy/deep_learning/covid_graph_model/images/current_covid_spread-animated.gif
 cd /home/happy/deep_learning/covid_graph_model-resume
 git pull
 time_stamp=$(date)
