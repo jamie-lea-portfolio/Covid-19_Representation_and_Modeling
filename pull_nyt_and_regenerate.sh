2a58cbceaa239c57d4edc1abaf72f52f5212ad64
jml-happy <17792773+jml-happy@users.noreply.github.com>
2020-04-12 07:45:15 -0500

    auto update animation

--- pull_nyt_and_regenerate.sh
+++ pull_nyt_and_regenerate.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 # this script sits in the private repository and is run by a cron job every day at 5:50am CST, it is also occassionally run manually
-PATH=$PATH:/home/happy/anaconda3/condabin:/usr/bin/python3:/usr/bin/git
 echo "Script started" >> /tmp/debug_cron
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate covid_graph
 cd /home/happy/deep_learning/covid_graph_model/nyt_repo && git pull
 cd /home/happy/deep_learning/covid_graph_model/python_scripts && python3 clean_nyt_county_data.py && python3 nytimes_render_spread-optimize.py
-conda deactivate
 cd /home/happy/deep_learning/covid_graph_model-resume/images
 rm current_covid_spread-animated.gif
 ln /home/happy/deep_learning/covid_graph_model/images/current_covid_spread-animated.gif
