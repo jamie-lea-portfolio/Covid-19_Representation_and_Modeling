d4287ac38e0ed3d10e07ce53af54d8e41cc8ec58
jml-happy <17792773+jml-happy@users.noreply.github.com>
2020-04-12 08:38:18 -0500

    auto update animation

--- pull_nyt_and_regenerate.sh
+++ pull_nyt_and_regenerate.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 # this script sits in the private repository and is run by a cron job every day at 5:50am CST, it is also occassionally run manually
-echo "Script started" >> /tmp/debug_cron
+time_stamp=$(date)
+echo "Script started at $time_stamp\n" >> /tmp/debug_cron
 # get plotly orca to work
 export DISPLAY=:0
-# use keychain so the ssh key doesn't have to be unlocked
-. ~/.keychain/`/bin/hostname`-sh
-# cd /home/happy/deep_learning/covid_graph_model/nyt_repo && git pull
-# cd /home/happy/deep_learning/covid_graph_model/python_scripts && python3 clean_nyt_county_data.py && python3 nytimes_render_spread-optimize.py
-# cd /home/happy/deep_learning/covid_graph_model-resume/images
-# rm current_covid_spread-animated.gif
-# ln /home/happy/deep_learning/covid_graph_model/images/current_covid_spread-animated.gif
+cd /home/happy/deep_learning/covid_graph_model/nyt_repo && git pull
+cd /home/happy/deep_learning/covid_graph_model/python_scripts
+python3 clean_nyt_county_data.py
+python3 nytimes_render_spread-optimize.py
+cd /home/happy/deep_learning/covid_graph_model-resume/images
+rm current_covid_spread-animated.gif
+ln /home/happy/deep_learning/covid_graph_model/images/current_covid_spread-animated.gif
 cd /home/happy/deep_learning/covid_graph_model-resume
 git pull
 time_stamp=$(date)
@@ -17,3 +18,5 @@ echo "updating at $time_stamp\n" >> auto_update.log
 git add *
 git commit -m "auto update animation"
 git push
+time_stamp=$(date)
+echo "Script finished started $time_stamp\n" >> /tmp/debug_cron
